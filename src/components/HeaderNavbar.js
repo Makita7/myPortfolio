e6e9763b2c1eb32b12af3624f7077e9be13ce976
Makita7 <sophiamakita@gamil.com>
2022-10-15 12:38:43 -0300

    navbar finished

--- src/components/HeaderNavbar.js
+++ src/components/HeaderNavbar.js
@@ -1,15 +1,45 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+    height: 5rem;
+    background-color: white;
+    position: sticky;
+    text-transform: uppercase;
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.2);
+`
+
+const Logo = styled.p`
+    font-family: 'Kanit', sans-serif;
+    font-weight: 500;
+    font-size: 1.7rem;
+    padding-left: 2rem;
+    color: #26A0A0;
+    margin-top: 1.4rem !important;
+`
+
+const Links = styled.div`
+    padding-right: 2rem;
+    padding-top: 1.8rem;
+    & a{
+        padding-right: 1rem;
+        font-family: 'Quicksand', sans-serif;
+        font-weight: 600;
+        text-decoration: none;
+    }
+`
+
 
 function SiteHeaderNavbar() {
     return (
-        <nav className='flex space-between'>
-            <p>Sofia Makita</p>
-            <div>
-                <Link to='/' >Home</Link>
-                <Link to='/portfolio' >Portfolio</Link>
-                <Link to='/about' >About</Link>
-            </div>
-        </nav>
+        <Navbar className='flex space-between'>
+            <Logo>Sofia Makita</Logo>
+            <Links>
+                <Link className='yellow' to='/' >Home</Link>
+                <Link className='olive' to='/portfolio' >Portfolio</Link>
+                <Link className='cobalt' to='/about' >About</Link>
+            </Links>
+        </Navbar>
     );
 }
 
